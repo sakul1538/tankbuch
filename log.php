4c9bb5e6e7826fb340786d795c0bb8dc671548ca
=Lukas Grossen <admin@dilexa.com>
2026-05-31 15:16:59 +0200

    Aktivitätenlog

--- log.php
+++ log.php
@@ -6,32 +6,33 @@ include_once 'sql_conn.php';
 function write_log($message,$typ)
 
 {
-
-    $datum = date('d.m.Y');
-    $zeit = date('H:m:s');
-    $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'unknown';
-    $ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'unknown';
-    $remote_host = isset($_SERVER['REMOTE_HOST']) ? $_SERVER['REMOTE_HOST'] : 'unknown';
-    $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 'guest';
-    $userName = isset($_SESSION['username']) ? $_SESSION['username'] : 'guest';
-
-
-
-    $pdo = connect_pdo();
-    $sql = "INSERT INTO " . TB_LOG . " (DATUM, ZEIT,IP,REF,REM_HOST,USER_ID,USER_NAME,MESSAGE,TYP) VALUES (:datum, :zeit, :ip, :ref, :remote_host, :user_id, :user_name, :message, :typ)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':datum', $datum);
-    $stmt->bindParam(':zeit', $zeit);
-    $stmt->bindParam(':user_id', $user_id);
-    $stmt->bindParam(':ip', $ip);
-    $stmt->bindParam(':typ', $typ);
-    $stmt->bindParam(':message', $message);
-    $stmt->bindParam(':ref', $ref);
-    $stmt->bindParam(':remote_host', $remote_host);
-    $stmt->bindParam(':user_name', $userName);
-    $stmt->execute();
-    $stmt = null;
-    $pdo = null;
+    try {
+        $datum = date('d.m.Y');
+        $zeit = date('H:m:s');
+        $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'unknown';
+        $ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'unknown';
+        $remote_host = isset($_SERVER['REMOTE_HOST']) ? $_SERVER['REMOTE_HOST'] : 'unknown';
+        $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 'guest';
+        $userName = isset($_SESSION['username']) ? $_SESSION['username'] : 'guest';
+        $pdo = connect_pdo();
+        $sql = "INSERT INTO " . TB_LOG . " (DATUM, ZEIT,IP,REF,REM_HOST,USER_ID,USER_NAME,MESSAGE,TYP) VALUES (:datum, :zeit, :ip, :ref, :remote_host, :user_id, :user_name, :message, :typ)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':datum', $datum);
+        $stmt->bindParam(':zeit', $zeit);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':ip', $ip);
+        $stmt->bindParam(':typ', $typ);
+        $stmt->bindParam(':message', $message);
+        $stmt->bindParam(':ref', $ref);
+        $stmt->bindParam(':remote_host', $remote_host);
+        $stmt->bindParam(':user_name', $userName);
+        $stmt->execute();
+        $stmt = null;
+        $pdo = null;
+    } catch (Exception $e)
+    {
+        echo 'Error writing log: ' . $e->getMessage();
+    }
 
 }
 
